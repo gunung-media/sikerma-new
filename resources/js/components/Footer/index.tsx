a2c404219bcadf22490c868ecdd6a9cb9e861e09
Richie-Z <zakariarichie@yahoo.com>
2025-01-23 15:15:21 +0700

    feat: rm footer

--- resources/js/components/Footer/index.tsx
+++ resources/js/components/Footer/index.tsx
@@ -6,14 +6,6 @@ export const Footer = () => {
                 <div
                     className="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column"
                 >
-                    <div className="text-body">
-                        ©{year} , made with ❤️ by &nbsp;
-                        <a
-                            href="https://github.com/Richie-Z"
-                            target="_blank"
-                            className="footer-link"
-                        >Richie-Z</a >
-                    </div>
                 </div>
             </div>
         </footer>
