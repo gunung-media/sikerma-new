2b10f31cbb129c6d62d8a05e45eed3023fac5338
Richie-Z <zakariarichie@yahoo.com>
2025-05-26 11:00:11 +0200

    fix: change localization

--- resources/js/utils/Swal.ts
+++ resources/js/utils/Swal.ts
@@ -2,13 +2,13 @@ import Swal from "sweetalert2"
 
 export const confirmationDelete = async (callback: () => void) => {
     const result = await Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
+        title: 'Apakah kamu yakin?',
+        text: "Kamu tidak bisa mengembalikan data ini!",
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
+        confirmButtonText: 'Ya, yakin'
     })
 
     if (result.isConfirmed) {
